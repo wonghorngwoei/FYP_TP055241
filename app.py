16fa6c444a0270f8d47e1e161a17474ef8db33ac
hwwong <wonghorngwoei@gmail.com>
2023-06-07 01:45:54 +0800

    Added Places API for FindNearbyHospital Function

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+import requests
 
 app = Flask(__name__)
 
@@ -12,3 +13,23 @@ if __name__ == "__main__":
     app.run(debug=True)
 
 # $env:FLASK_DEBUG = "1" -- Run this before do your coding
+
+@app.route('/search', methods=['POST'])
+def search():
+    query = request.form['query']
+    api_key = 'AIzaSyDf-tSbLPvre8cFGB5IBGVK3PCwIAHxrJs'
+    url = f'https://maps.googleapis.com/maps/api/place/textsearch/json?query=hospitals+near+{query}&key={api_key}'
+
+    response = requests.get(url)
+    data = response.json()
+
+    # Extract the relevant information from the API response
+    results = data['results']
+    hospitals = []
+
+    for result in results:
+        name = result['name']
+        address = result['formatted_address']
+        hospitals.append({'name': name, 'address': address})
+
+    return render_template('results.html', hospitals=hospitals)
